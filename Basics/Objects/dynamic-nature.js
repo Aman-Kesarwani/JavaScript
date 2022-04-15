7c74dadba96de9b78467ffa0abd5400670aaa6e2
Aman-Kesarwani <amank2410@gmail.com>
2022-04-16 00:33:05 +0530

    dynamic nature

---
+++ Basics/Objects/dynamic-nature.js
@@ -0,0 +1,13 @@
+const circle  = {
+    radius: 1
+};
+
+circle.color = 'yellow';
+
+circle.draw = function(){}
+
+console.log(circle);
+
+delete circle.color;
+delete circle.draw;
+console.log(circle);
